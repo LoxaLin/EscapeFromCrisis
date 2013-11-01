f38ae59618f9abb626071d1422ab7ad8b8a369b2
LoxaLin <xup6vm054@gmail.com>
2013-11-01 15:10:12 +0800

    add routeplanning

--- gen/com/example/escapefromcrisis/R.java
+++ gen/com/example/escapefromcrisis/R.java
@@ -24,16 +24,27 @@ public final class R {
         public static final int ic_launcher=0x7f020000;
     }
     public static final class id {
-        public static final int action_settings=0x7f080000;
+        public static final int action_settings=0x7f080008;
+        public static final int iV_DestinationPlanning=0x7f080004;
+        public static final int iV_ReportToCrisis=0x7f080006;
+        public static final int iV_RoutePlanning=0x7f080002;
+        public static final int map=0x7f080007;
+        public static final int tV_Empty1=0x7f080001;
+        public static final int tV_Empty2=0x7f080003;
+        public static final int tV_Empty3=0x7f080005;
+        public static final int tV_Title=0x7f080000;
     }
     public static final class layout {
         public static final int activity_crisis=0x7f030000;
+        public static final int routeplanning=0x7f030001;
     }
     public static final class menu {
         public static final int crisis=0x7f070000;
     }
     public static final class string {
         public static final int action_settings=0x7f050001;
+        public static final int activity_main_empty=0x7f050004;
+        public static final int activity_main_title=0x7f050003;
         public static final int app_name=0x7f050000;
         public static final int hello_world=0x7f050002;
     }
---
+++ src/com/example/escapefromcrisis/RoutePlanning.java
@@ -0,0 +1,15 @@
+package com.example.escapefromcrisis;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class RoutePlanning extends Activity{
+//	private GoogleMap map;
+	
+	
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.routeplanning);
+	}
+}
