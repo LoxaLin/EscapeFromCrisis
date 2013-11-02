5f0afe76e0d0a545ed5f6bcec76f7fcbf364c9b1
LoxaLin <xup6vm054@gmail.com>
2013-11-02 14:45:13 +0800

    google map set up already
    AndroidManifest.xml -> <user-premission> set alreay
    RoutePlanning.java -> import alreay

--- src/com/example/escapefromcrisis/RoutePlanning.java
+++ src/com/example/escapefromcrisis/RoutePlanning.java
@@ -1,11 +1,18 @@
 package com.example.escapefromcrisis;
 
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.MapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Marker;
+import com.google.android.gms.maps.model.MarkerOptions;
+
 import android.app.Activity;
 import android.os.Bundle;
 
 public class RoutePlanning extends Activity{
 //	private GoogleMap map;
-	
+	private GoogleMap map;
 	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
